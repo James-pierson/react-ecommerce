06a203e3c6785f99594140e74029bf59c0b0c33f
James Pierson <jameswpbusiness@gmail.com>
2020-06-02 02:12:46 -0400

    set up all components and imported into app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import Navbar from './Components/Navbar'
+import Details from './Components/Details'
+import Cart from './Components/Cart'
+import ProductList from './Components/ProductList'
+import Default from './Components/Details'
 
 function App() {
   return (
     <React.Fragment>
-      <h3>hello from app</h3>
+      <Navbar />
+      <ProductList />
+      <Details />
+      <Cart />
+      <Default />
     </React.Fragment>
   );
 }
