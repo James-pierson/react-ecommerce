c344c1f0f38ef4caac2feee5ef29ac189a988291
James Pierson <jameswpbusiness@gmail.com>
2020-06-03 11:00:42 -0400

    fixed render in app component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
 import React, { Component } from 'react';
-import {Switch,Route} from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import Navbar from './Components/Navbar';
-import Cart from './Components/Cart';
-import Details from './Components/Details';
 import ProductList from './Components/ProductList';
+import Details from './Components/Details';
+import Cart from './Components/Cart';
 import Default from './Components/Default';
 
 class App extends Component {
-  return() {
+  render() {
     return (
       <React.Fragment>
         <Navbar />
         <Switch>
-          <Route path="/" component={ProductList}/>
-          <Route path="/details" component={Details}/>
-          <Route path="/cart" component={Cart}/>
-          <Route component={Default}/>
+          <Route exact path="/" component={ProductList} />
+          <Route path="/details" component={Details} />
+          <Route path="/cart" component={Cart} />
+          <Route component={Default} />
         </Switch>
-        <ProductList />
-        <Details />
-        <Cart />
-        <Default />
       </React.Fragment>
     )
   }
