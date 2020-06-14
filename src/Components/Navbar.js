eee145ccfa50c289ae7304b8fef5de1b64a4e659
James Pierson <jameswpbusiness@gmail.com>
2020-06-14 18:46:06 -0400

    setup button component

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -42,4 +42,12 @@ border-radius:0.5rem;
 padding: 0.2rem 0.5rem;
 cursor:pointer;
 margin:0.2rem 0.5rem 0.2rem 0;
+transition: all 0.5s ease-in-out;
+&:hover{
+    background: var(--lightBlue);
+    color: var(--mainBlue);
+}
+&:focus {
+    outline: none;
+}
 `
