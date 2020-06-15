b7c234436705a87a1d6d471a57bd048811c0e5f8
James Pierson <jameswpbusiness@gmail.com>
2020-06-14 21:14:31 -0400

    added store products data

--- src/Components/ProductList.js
+++ src/Components/ProductList.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react'
 import Product from './Product.js'
+import Title from './Title'
+import {storeProducts} from '../data'
+
 export default class ProductList extends Component {
+    state = {
+        products: storeProducts
+    }
     render() {
         return (
-            <div>
-                <Product/>
-            </div>
+            <React.Fragment>
+                <div className="py-5">
+                    <div className="container">
+                        <Title name="our" title="products" />
+                        <div classname="row">
+                        </div>
+                    </div>
+                </div>
+            </React.Fragment>
         )
     }
 }
