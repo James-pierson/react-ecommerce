6f522d5cf5108596fa0480d75bc033c8c6be5afa
James Pierson <jameswpbusiness@gmail.com>
2020-06-14 22:13:14 -0400

    adding more components

--- src/Components/ProductList.js
+++ src/Components/ProductList.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Product from './Product.js'
 import Title from './Title'
 import {storeProducts} from '../data'
+import {ProductConsumer} from '../context';
 
 export default class ProductList extends Component {
     state = {
@@ -14,6 +15,11 @@ export default class ProductList extends Component {
                     <div className="container">
                         <Title name="our" title="products" />
                         <div classname="row">
+                        <ProductConsumer>
+                            {(value)=> {
+                                return <h1>{value}</h1>
+                            }}
+                        </ProductConsumer>
                         </div>
                     </div>
                 </div>
