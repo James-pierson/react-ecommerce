9d1e83afb4702f03e609bb78f20cdffdc307fdba
James Pierson <jameswpbusiness@gmail.com>
2020-07-07 21:59:57 -0400

    continuting project

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import styled from 'styled-components'
+import styled from 'styled-components';
 import logo from '../logo.svg';
 import { ButtonContainer } from './Button';
 
--- src/Components/ProductList.js
+++ src/Components/ProductList.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react'
-import Product from './Product.js'
-import Title from './Title'
-import {storeProducts} from '../data'
+import React, { Component } from 'react';
+import Product from './Product.js';
+import Title from './Title';
+import {storeProducts} from '../data';
 import {ProductConsumer} from '../context';
 
 export default class ProductList extends Component {
